4d64e4765d132985a9bc9497d9894d8fad59c445
Arjun Tyagi <arjuntyagi1707@gmail.com>
2025-09-08 12:01:28 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+} 
