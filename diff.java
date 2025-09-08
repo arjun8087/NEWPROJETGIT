a56a02943cb4937cc2479efe7f419e00ea45c27f
Arjun Tyagi <arjuntyagi1707@gmail.com>
2025-09-08 16:52:08 +0530

    added implementation for diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 public class diff{
-
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int diff = a - b;
+        System.out.println("the difference is" + diff);
+    }
 }
